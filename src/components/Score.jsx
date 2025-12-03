c54e1834f61b4d366f2894f917f028d6467c7709
Jere Zarza <jerezarza98@gmail.com>
2025-12-03 01:10:56 -0300

    agrego funcionalidad para jugar una nueva partida

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,11 +1,19 @@
+import { useNavigate } from 'react-router-dom'
 import './Score.css'
 
 export default function Score({ score, difficulty }) {
+	const navigate = useNavigate()
+
+	const handleClick = () => navigate('/')
+
 	return (
 		<div className='score'>
 			<h1 className='score__title'>
 				Your score is {score} on {difficulty} difficulty.
 			</h1>
+			<button className='score__button' onClick={handleClick}>
+				Play again
+			</button>
 		</div>
 	)
 }
