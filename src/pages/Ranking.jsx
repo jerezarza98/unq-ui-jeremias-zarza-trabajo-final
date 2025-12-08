59c29644ad6b3f5870db9c44a4f8519361b28c37
Jere Zarza <jerezarza98@gmail.com>
2025-12-08 15:55:43 -0300

    agrego estilos a Ranking

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { getRanking } from '../logic/storage'
+import './Ranking.css'
 
 export default function Ranking() {
 	const [difficulty, setDifficulty] = useState('easy')
@@ -7,26 +8,53 @@ export default function Ranking() {
 
 	const filteredRanking = ranking.filter(user => user.difficulty === difficulty)
 	return (
-		<div>
-			<h1>Ranking</h1>
-			<div>
-				<button onClick={() => setDifficulty('easy')}>easy</button>
-				<button onClick={() => setDifficulty('normal')}>normal</button>
-				<button onClick={() => setDifficulty('hard')}>hard</button>
-				<button onClick={() => setDifficulty('extreme')}>extreme</button>
+		<div className='ranking'>
+			<h1 className='ranking__title'>Ranking</h1>
+			<div className='ranking__buttons'>
+				<button
+					className='ranking__button'
+					onClick={() => setDifficulty('easy')}>
+					easy
+				</button>
+				<button
+					className='ranking__button'
+					onClick={() => setDifficulty('normal')}>
+					normal
+				</button>
+				<button
+					className='ranking__button'
+					onClick={() => setDifficulty('hard')}>
+					hard
+				</button>
+				<button
+					className='ranking__button'
+					onClick={() => setDifficulty('extreme')}>
+					extreme
+				</button>
 			</div>
 			{filteredRanking.length === 0 ? (
-				<p>Empty</p>
+				<div className='ranking__empty-container'>
+					<p className='ranking__empty'>Empty</p>
+				</div>
 			) : (
-				<div>
-					<p>{difficulty}</p>
-					{filteredRanking.map(user => (
-						<div key={`${user.name}-${user.difficulty}`}>
-							<p>{user.name}</p>
-							<p>{user.score}</p>
-							<p>{user.difficulty}</p>
-						</div>
-					))}
+				<div className='ranking__container'>
+					<p className='ranking__difficulty'>{difficulty}</p>
+					<div className='ranking__items'>
+						{filteredRanking.map((user, index) => (
+							<div
+								key={`${user.name}-${user.difficulty}`}
+								className='ranking__item'>
+								<div className='ranking__rank-name'>
+									<p className='ranking__rank'>{index + 1}</p>
+									<p className='ranking__name'>{user.name}</p>
+								</div>
+
+								<p className='ranking__score'>
+									{user.score} <span>Score</span>
+								</p>
+							</div>
+						))}
+					</div>
 				</div>
 			)}
 		</div>
